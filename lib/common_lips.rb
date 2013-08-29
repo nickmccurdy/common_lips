8dfb1ca4319182fff68e1cc8ce0da9159b8920d3
Nicolas McCurdy <thenickperson@gmail.com>
2013-08-28 21:49:43 -0400

    Implement parenthesis swapping

--- lib/common_lips.rb
+++ lib/common_lips.rb
@@ -1,3 +1,18 @@
 module CommonLips
   VERSION = '0.0.1'
+
+  REPLACEMENTS = {
+    ')' => '(',
+    '(' => ')'
+  }
+
+  def self.swap_parens(code)
+    map_string(code) { |char| REPLACEMENTS[char] || char }
+  end
+
+  private
+
+  def self.map_string(str, &block)
+    str.split(//).map(&block).join
+  end
 end
--- spec/common_lips_spec.rb
+++ spec/common_lips_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe CommonLips do
+  before :all do
+    LIPS_FILE = File.open('spec/samples/fib.lips', 'r').read
+    LISP_FILE = File.open('spec/samples/fib.lisp', 'r').read
+  end
+
   it 'should have a version number' do
     CommonLips::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should be_true
+  it 'should convert Lips code to Lisp' do
+    expect(CommonLips.swap_parens(LIPS_FILE)).to eq LISP_FILE
+  end
+
+  it 'should convert Lisp code to Lips' do
+    expect(CommonLips.swap_parens(LISP_FILE)).to eq LIPS_FILE
   end
 end
