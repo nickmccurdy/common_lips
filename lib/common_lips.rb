c4afc2253f43db20a8267d3fee35192f1414879d
Nicolas McCurdy <thenickperson@gmail.com>
2013-08-30 15:25:49 -0400

    Add documentation to the CommonLips module

--- lib/common_lips.rb
+++ lib/common_lips.rb
@@ -1,20 +1,35 @@
+# The central module for CommonLips
 module CommonLips
 
   extend self
 
+  # The version of the CommonLips gem
   VERSION = '0.0.1'
 
+  # A hash of characters that should be replaced when converting between Lips
+  # and Lisp (and vice versa)
   REPLACEMENTS = {
     ')' => '(',
     '(' => ')'
   }
 
+  # Swaps all left and right parenthesis in a String
+  #
+  # @param [String] code a String of source code to convert
+  #
+  # @return [String] the converted source code
   def swap_parens(code)
     map_string(code) { |char| REPLACEMENTS[char] || char }
   end
 
   private
 
+  # Lets you map over a String as if it were Enumerable
+  #
+  # @param [String] str the String to map over
+  # @yield a block that changes each character in the String
+  #
+  # @return the resulting String (after mapping each of its characters)
   def map_string(str, &block)
     str.split(//).map(&block).join
   end
