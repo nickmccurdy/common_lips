f0a8b8a58960bc298752c0d13fc12af497f93f7c
Nicolas McCurdy <thenickperson@gmail.com>
2013-08-30 13:46:00 -0400

    RSpec syntax tweaks

--- spec/common_lips_spec.rb
+++ spec/common_lips_spec.rb
@@ -6,15 +6,15 @@ describe CommonLips do
     LISP_FILE = File.open('spec/samples/fib.lisp', 'r').read
   end
 
-  it 'should have a version number' do
-    CommonLips::VERSION.should_not be_nil
+  it 'has a version number' do
+    expect(CommonLips::VERSION).to_not be_nil
   end
 
-  it 'should convert Lips code to Lisp' do
+  it 'converts Lips code to Lisp' do
     expect(CommonLips.swap_parens(LIPS_FILE)).to eq LISP_FILE
   end
 
-  it 'should convert Lisp code to Lips' do
+  it 'converts Lisp code to Lips' do
     expect(CommonLips.swap_parens(LISP_FILE)).to eq LIPS_FILE
   end
 end
